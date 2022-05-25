96a4bf39daba30500c0f2d6b71b14d995ca297bb
OscarCruzLopez <oscarandrecruzlopez@gmail.com>
2022-05-25 11:36:48 +0200

    commit Final

--- src/main/java/agenda/Agenda.java
+++ src/main/java/agenda/Agenda.java
@@ -15,7 +15,12 @@ public class Agenda {
     private HashMap<String, Contacto> contactos = new HashMap();
     private static int n = 0;
     private static int count = 0;
- 
+ /**
+  * Añade un contacto a la agenda
+  * @param contacto contacto que tenemos que añadir
+  * @return objeto contacto añadido o genera una excepcion si ya existia
+  * @throws ContactoRepetidoException Excepcion, ver paquete exception
+  */
     public Contacto addContacto(Contacto contacto) throws ContactoRepetidoException {
         if (contactos.containsKey(contacto.getEmail())) {
             //
@@ -27,7 +32,11 @@ public class Agenda {
         count++;
         return contacto;
     }
-
+/**
+ * correo electronico que queremos eliminar
+ * @param email
+ * @return 
+ */
     public Contacto eliminarContacto(String email) { 
         Contacto contacto = buscarContacto(email);
         if (contacto != null) {
@@ -36,7 +45,11 @@ public class Agenda {
         }
         return contacto;
     }
-
+/**
+ * busca los contactos de la agenda
+ * @param email
+ * @return 
+ */
     public Contacto buscarContacto(String email) { 
         Contacto contacto = null;
         if (!contactos.containsKey(email)) {
@@ -47,7 +60,10 @@ public class Agenda {
         return contactos.get(email);
         }
     }
-
+/**
+ * coge los contactos de la agenda
+ * @return 
+ */
     public ArrayList<Contacto> getContactos() {
         ArrayList<Contacto> contactosList = new ArrayList<>();
         contactosList.addAll(contactos.values());
